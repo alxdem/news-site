228b344bb8c386b3ed70be32b97103946d5dfd3f
AlexeyD <dr.a.lekter@gmail.com>
2022-06-27 22:27:41 +0300

    Create the socials use an own Web Component

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/utils.js', mode: 'client' },
-    { src: '~/plugins/swiper.js', mode: 'client' }
+    { src: '~/plugins/swiper.js', mode: 'client' },
+    { src: '~/plugins/chat.js', mode: 'client'},
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
